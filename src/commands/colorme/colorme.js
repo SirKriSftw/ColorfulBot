e32e8310fa63e33de7afcce85401170cd2c12a0c
Kristian Perez <SirKriSftw@hotmail.com>
2024-06-21 13:15:59 -0400

    Command now checks if input is a valid hex, if its not return an error to user

--- src/commands/colorme/colorme.js
+++ src/commands/colorme/colorme.js
@@ -1,4 +1,9 @@
-const { ApplicationCommandOptionType } = require('discord.js');
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+
+
+
+// Regular expression for hex color code validation
+const hexColorRegex = /^#([0-9A-F]{3}){1,2}$/i;
 
 module.exports = {
     name: 'colorme',
@@ -13,6 +18,20 @@ module.exports = {
     ],
 
     callback: (client, interaction) => {
-        interaction.reply('Test!')
+        const colorCode = interaction.options.getString('color-code');
+
+        if (!hexColorRegex.test(colorCode))
+        {
+            const embed = new EmbedBuilder()
+                .setColor('#FF0000')
+                .setDescription('Error: Invalid hex color code format. Please use #RRGGBB');
+
+            return interaction.reply(
+                {
+                    embeds: [embed],
+                    ephemeral: true
+                }                
+            );
+        }
     }
 }
