b3f2258275223865a45a321a53beda562ff955c8
Kristian Perez <SirKriSftw@hotmail.com>
2024-06-25 23:14:24 -0400

    If a user puts in the same hex as their existing hex, it just removes their role and tells them

--- src/commands/colorme/colorme.js
+++ src/commands/colorme/colorme.js
@@ -10,12 +10,12 @@ module.exports = {
     options: [
         {
             name: 'color-code',
-            description: 'Hex color code to change to',
+            description: 'Color Name or Hex to change to (Ability to use hex is controlled by server admins)',
             required: true,
             type: ApplicationCommandOptionType.String
         }
     ],
-
+    deleted: false,
     callback: async (client, interaction) => {
         const colorCode = interaction.options.getString('color-code').toUpperCase();
 
@@ -32,9 +32,10 @@ module.exports = {
 
         let colorMeRole = interaction.guild.roles.cache.find(role => role.name.toUpperCase().includes('COLOR') && role.name.toUpperCase() != "COLORFULBOT");
         let position = colorMeRole ? colorMeRole.position : interaction.guild.roles.size;
-
+        
         if(!role)
         {
+            console.log('Brand new role')
             try
             {
                 role = await interaction.guild.roles.create({
@@ -63,16 +64,61 @@ module.exports = {
         {
             interaction.member.roles.add(role);
 
-            const msg = `Assigned role ${colorCode}.`
-                sendMsg(
-                    interaction, 
-                    msg, 
-                    '#00FF00');
+            if(existingColorRole)
+            {
+                console.log(existingColorRole.color + " " + colorCode)
+                if(existingColorRole.name === colorCode)
+                {
+                    handleSameColor(interaction, existingColorRole, colorCode);
+                }
+                else
+                {
+                    const msg = `Assigned role ${colorCode}.`
+                    sendMsg(
+                        interaction, 
+                        msg, 
+                        '#00FF00');
 
-            handleExistingColor(interaction, existingColorRole);
+                    handleExistingColor(interaction, existingColorRole);
+                }
+            }
+            
         }
     }
 };
+
+async function handleSameColor(interaction, existingColorRole, colorCode) {
+    if(existingColorRole) 
+    {
+        if(existingColorRole.name === colorCode)
+        {
+            try 
+            {
+                const membersCount = existingColorRole.members.size
+                await interaction.member.roles.remove(existingColorRole, 'Removing color');
+                const msg = `Removed ${colorCode}.`
+
+                if (membersCount - 1 <= 0)
+                {
+                    await existingColorRole.delete('Role is empty now');
+                    console.log(`Deleting Role ${existingColorRole.name}`);
+                }
+
+                return sendMsg(
+                    interaction, 
+                    msg, 
+                    '#FF0000');
+            }
+            catch (error)
+            {
+                console.log(error);
+            }
+        }
+
+    }
+    return false;
+}
+
 
 async function handleExistingColor(interaction, existingColorRole) {
     if (existingColorRole)
