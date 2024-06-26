5fdd1ac2446ead170f7b45ecfd0b158e352b946c
Kristian Perez <SirKriSftw@hotmail.com>
2024-06-26 00:45:11 -0400

    Updated help cmd

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -7,8 +7,10 @@ module.exports = {
     callback: async (client, interaction) => {
         sendMsg(
             interaction,
-            'To change your color just simply run /colorme #<your hex value here>.\n' +
-            'If you are a server admin and want to organize created colors just include a role that has \'color\' somewhere in it (case insensitive and ignores colorfulbot role)',
+            'To change your color just simply run /colorme #<your hex value here> OR <color name here>.\n' +
+            'If you are a server admin and want to organize created colors just include a role that has \'color\' somewhere in it (case insensitive and ignores colorfulbot role)\n' +
+            'If you wish for only CERTAIN users to be able to make hex colors include the following roles: \'color\' \'color end\' \'hexable\'.\n'+
+            'color and color end roles can have characters inbetween but hexable must be exaclty hexable. Users assigned with hexable can use hex values for colors.',
             '#FFFFFF',
             -1);
     }
